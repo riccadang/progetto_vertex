6a2e23bb84853c98342c9694acff72a4b91425ad
riccardocecere <ric.cecere1@stud.uniroma3.it>
2019-04-07 19:46:07 +0200

    aggiunto il secondo step ma non funziona per quaòche motivo; forse sbaglio i passaggi dei parametri per indirizzo

--- structural_clustering.py
+++ structural_clustering.py
@@ -1,6 +1,7 @@
 from collections import defaultdict
 import generate_masked_vector
 from tqdm import tqdm
+import masked_shingle_vector_service as masked_service
 
 def create_masked(vector):
     all_masked = []
@@ -10,8 +11,6 @@ def create_masked(vector):
     all_masked = all_masked + six_eight_io
     all_masked = all_masked + seven_eight_io
     return all_masked
-
-
 
 #FIRST PART
 def first_part():
@@ -31,6 +30,26 @@ def first_part():
             dict[masked]= dict[masked]+1
     return dict
 
+#SECOND PART
+def second_part(dict, pages):
+    dict_masked_covered = defaultdict(int)
+    file = open(pages, "r", encoding='utf-8')
+    vectors = []
+    #get vectors
+    for x in file:
+        tmp = x.rstrip('\n').split(sep='\t')
+        vectors.append(tmp[1])
+    #for each vector get the set of masked shingle vectors that covers it an decrement their counts except the leader one (the one with the most counts)
+    for vector in vectors:
+        dict_masked_covered = masked_service.get_masked_vectors_that_covers_vector(dict, vector)
+        dict_masked_covered = masked_service.decrement_counts(dict_masked_covered)
+        #update the dictionary of masked shingle vectors
+        for key, value in dict_masked_covered:
+            dict[key] = value
+    return dict
+
 if __name__ == '__main__':
     dict = first_part()
-    print (dict)
+    print(dict)
+    dict_normalized = second_part(dict, "page_vector_elena.txt")
+    print (dict_normalized)
